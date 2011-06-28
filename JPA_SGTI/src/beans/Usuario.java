73f785e7fec8127113d7f7238ef5441f0bffc7a4
plindiman <plindiman@localhost6.localdomain6>
2011-06-28 10:56:35 -0300

    modifique nombreCompleto

---
+++ JPA_SGTI/src/beans/Usuario.java
@@ -0,0 +1,84 @@
+package beans;
+
+import java.io.Serializable;
+import java.lang.Long;
+import java.lang.String;
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: Usuario
+ *
+ */
+@Entity
+
+public class Usuario implements Serializable {
+
+	   
+	@Id
+	private Long cedula;
+	private String nombre;
+	private String apellido;//hice esta modificación desde mi PC
+	private String usuario;
+	private String pwd;
+	private String telefono;
+	private String direccion;
+	private String celular;
+	private static final long serialVersionUID = 1L;
+
+	public Usuario() {
+		super();
+	}   
+	public Long getCedula() {
+		return this.cedula;
+	}
+
+	public void setCedula(Long cedula) {
+		this.cedula = cedula;
+	}   
+	public String getNombreCompleto() {
+		return this.nombreCompleto;
+	}
+
+	public void setNombreCompleto(String nombreCompleto) {
+		this.nombreCompleto = nombreCompleto;
+	}   
+	public String getUsuario() {
+		return this.usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}   
+	public String getPwd() {
+		return this.pwd;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}   
+	public String getTelefono() {
+		return this.telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}   
+	public String getDireccion() {
+		return this.direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}   
+	public String getCelular() {
+		return this.celular;
+	}
+
+	public void setCelular(String celular) {
+		this.celular = celular;
+	}
+   
+}
+
+
+//estoy editando el archivo desde la web
