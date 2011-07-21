df62e4a86b1f217d80592ae57ad8b894e8ad6eb4
plindiman <plindiman@localhost6.localdomain6>
2011-07-21 15:59:34 -0300

    Prueba de ingreso de datos de  USUARIOTecnico, Encargado, Administrador, GRUPO, ESTADOS, TIPO Y TAREA

--- SGTI_JPA/src/beans/Tipo.java
+++ SGTI_JPA/src/beans/Tipo.java
@@ -8,6 +8,7 @@ import javax.persistence.*;
  * Entity implementation class for Entity: Tipo
  *
  */
+
 @Entity
 
 public class Tipo implements Serializable {
@@ -16,9 +17,10 @@ public class Tipo implements Serializable {
 	@Id
 	private int id;
 	private String descripcion;
-//	private ColTareas colTareas;
+//	private ColTareas colTareas;	
 	private static final long serialVersionUID = 1L;
 
+	
 	public Tipo() {
 		super();
 	}   
