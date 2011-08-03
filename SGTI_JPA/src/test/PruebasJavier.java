7a50be773f38ecdaebe2d30e176751c6a1fb72b8
reivaj <reivaj@reivaj-AOA150.lan>
2011-08-03 18:31:48 -0300

    Cambios en manager y asignacion de tarea a usuario.

---
+++ SGTI_JPA/src/test/PruebasJavier.java
@@ -0,0 +1,47 @@
+package test;
+
+import java.util.Calendar;
+
+import manager.ManagerTarea;
+import manager.ManagerUsuario;
+import beans.Tarea;
+import beans.Tipo;
+import beans.Usuario;
+
+public class PruebasJavier {
+	
+	public static void main(String[] args) {
+	
+		ManagerTarea mt = new ManagerTarea();
+		Tarea t= new Tarea();
+		//Tipo tipo= new Tipo();
+		//tipo.setDescripcion("TIPO DE TAREA");
+		t.setEsExterna(true);	
+		t.setDescripcion("Soporte tecnico a la Empresa X: revisar maquina en garantía");
+		t.setObservacion("Observacion de la tarea");
+		t.setFechaApertura(Calendar.getInstance());
+		t.setFechaComprometida(Calendar.getInstance());
+		//t.setTipo(tipo);
+		if (mt.altaTarea(t))
+			System.out.println("TAREA DADA DE ALTA");
+		else
+			System.out.println("ERROR AL DAR DE ALTA LA TAREA");
+		
+		ManagerUsuario mu= new ManagerUsuario();
+		Usuario u = new Usuario();
+		u.setCedula(40434685);
+		u.setNombre("Javier");
+		u.setApellido("Maly");
+		u.setUsuario("usu");
+		u.setPwd("pwd");
+		u.setDireccion("Paysandu 1242 / 203");
+		u.setCelular("099722146");
+		mu.altaUsuario(u);
+		
+		//asigno tarea
+		
+		mt.asignaTareaUsuario(t, u, Calendar.getInstance());
+		
+	}
+
+}
