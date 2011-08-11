f77bb6395abbafb960898f448258113a48c67bdb
reivaj <reivaj@reivaj-AOA150.lan>
2011-08-10 22:36:19 -0300

    EJB Manager Tarea + pruebas

--- SGTI_JPA/src/manager/ManagerTarea.java
+++ SGTI_JPA/src/manager/ManagerTarea.java
@@ -18,16 +18,16 @@ import beans.Usuario;
 
 public class ManagerTarea {
 
-//	DBConection db = new DBConection();
-//	EntityManager em = db.conectar();
+	// DBConection db = new DBConection();
+	// EntityManager em = db.conectar();
 
-	public boolean altaTarea(EntityManager em ,Tarea t, Tipo tipo, Tiene tiene) {
+	public boolean altaTarea(EntityManager em, Tarea t, Tipo tipo, Tiene tiene) {
 		try {
-			
+
 			em.persist(tipo);
 			em.persist(tiene);
 			em.persist(t);
-		
+
 			return true;
 
 		} catch (Exception e) {
@@ -37,19 +37,19 @@ public class ManagerTarea {
 
 	}
 
-	public List<Tarea> traerTodasTareas(EntityManager em ) {
+	public List<Tarea> traerTodasTareas(EntityManager em) {
 		// para que deje de mostrar advertencia List need unchecked convertion
 		@SuppressWarnings(value = "unchecked")
 		List<Tarea> todasT = em.createNamedQuery("todosTareas").getResultList();
 		return todasT;
 	}
 
-	public Tarea encontrarTarea(EntityManager em ,int id) {
+	public Tarea encontrarTarea(EntityManager em, int id) {
 		Tarea t = em.find(Tarea.class, id);
 		return t;
 	}
 
-	public List<Tarea> tareasPorUsuario(EntityManager em ,Usuario u) {
+	public List<Tarea> tareasPorUsuario(EntityManager em, Usuario u) {
 		@SuppressWarnings(value = "unchecked")
 		List<Tarea> tareas = em.createNamedQuery("tareasPorUsuario")
 				.setParameter("Usuario", u).getResultList();
@@ -57,30 +57,42 @@ public class ManagerTarea {
 	}
 
 	// ACTUALIZAR TAREA
-	public Tarea actualizarTarea(EntityManager em , Tarea t) {
-		
+	public Tarea actualizarTarea(EntityManager em, Tarea t) {
+
 		t = em.merge(t);
-		
+
 		return t;
 	}
 
 	// ELIMINAR TAREA
-	public void eliminarTarea(EntityManager em , Tarea t) {
+	public boolean eliminarTarea(EntityManager em , Tarea t) {
 		
-		em.remove(t);
-		
+		try{
+			em.remove(t);
+			return true;
+		}
+		catch(Exception e){
+			e.printStackTrace();
+			return false;
+		}
 	}
 
-	public void altaTareaRealiza(EntityManager em , Tarea t, Realiza r) {
-	
+	public boolean altaTareaRealiza(EntityManager em, Tarea t, Realiza r) {
+
+		try{
 		em.persist(t);
 		em.persist(r);
-	
-
+		return true;
+		}
+		catch(Exception e){
+			e.printStackTrace();
+			return false;
+		}
 	}
 
 	// ASIGNAR TAREA A USUARIO
-	public boolean asignaTareaUsuario(EntityManager em , Tarea t, Usuario u, Calendar fecIni) {
+	public boolean asignaTareaUsuario(EntityManager em, Tarea t, Usuario u,
+			Calendar fecIni) {
 		boolean retorno = false;
 		Realiza r = new Realiza();
 		r.setTarea(t);
@@ -97,11 +109,11 @@ public class ManagerTarea {
 	}
 
 	// ALTA GRUPO
-	public boolean altaGrupo(EntityManager em , Grupo gr) {
+	public boolean altaGrupo(EntityManager em, Grupo gr) {
 		try {
-			
+
 			em.persist(gr);
-			
+
 			return true;
 
 		} catch (Exception e) {
@@ -112,16 +124,16 @@ public class ManagerTarea {
 	}
 
 	// ENCONTRAR GRUPO
-	public Grupo encontrarGrupo(EntityManager em , int id) {
+	public Grupo encontrarGrupo(EntityManager em, int id) {
 		Grupo gru = em.find(Grupo.class, id);
 		return gru;
 	}
 
 	// ACTUALIZAR GRUPO
-	public Grupo actualizarGrupo(EntityManager em , Grupo gr) {
-		
+	public Grupo actualizarGrupo(EntityManager em, Grupo gr) {
+
 		gr = em.merge(gr);
-		
+
 		return gr;
 	}
 
@@ -129,7 +141,7 @@ public class ManagerTarea {
 	// pasar un TIENE para que cambie el estado de la tarea
 	// seria el caso de cuando se abre la tarea y se la asigna a un grupo
 	// se pasa un tiene porque la tarea queda en estado ABIERTA.
-	public boolean asignarTareaGrupo(EntityManager em , Tarea t, Grupo gr) {
+	public boolean asignarTareaGrupo(EntityManager em, Tarea t, Grupo gr) {
 		boolean retorno = false;
 		if (gr.asignaTarea(t))
 			actualizarGrupo(em, gr);
@@ -139,55 +151,68 @@ public class ManagerTarea {
 	}
 
 	// traer un estado
-	public Estado encontrarEstado(EntityManager em , int id) {
+	public Estado encontrarEstado(EntityManager em, int id) {
 		Estado e = em.find(Estado.class, id);
 		return e;
 	}
 
 	// OBTIENE EL TIENE SIN FINALIZAR DE UNA TAREA
 	public Tiene tieneDeTarea(Tarea t) {
-		List<Tiene> colTiene=t.getColTiene();
-		Tiene tiene=null;
-		Tiene tieneIt=null;
+		List<Tiene> colTiene = t.getColTiene();
+		Tiene tiene = null;
+		Tiene tieneIt = null;
 		Iterator iter = colTiene.iterator();
-		while (iter.hasNext()){
-			tieneIt=(Tiene)iter.next();
-			if(tieneIt.getFechaFin()==null){
-				tiene=tieneIt;
-			
+		while (iter.hasNext()) {
+			tieneIt = (Tiene) iter.next();
+			if (tieneIt.getFechaFin() == null) {
+				tiene = tieneIt;
+
 			}
-					  
+
 		}
 		return tiene;
 	}
 
 	// CAMBIAR ESTADO A TAREA
-	public boolean cambiarEstadoTarea(EntityManager em ,Tarea t, Estado est) {//aca en vez de pasarle el estado podriamos pasarle el id dl estado
+	public boolean cambiarEstadoTarea(EntityManager em, Tarea t, Estado est) {// aca
+																				// en
+																				// vez
+																				// de
+																				// pasarle
+																				// el
+																				// estado
+																				// podriamos
+																				// pasarle
+																				// el
+																				// id
+																				// dl
+																				// estado
 		boolean retorno = false;
-		Tiene ti=tieneDeTarea(t);
-		if(ti!=null){
+		Tiene ti = tieneDeTarea(t);
+		if (ti != null) {
 			ti.setFechaFin(Calendar.getInstance());
 			actualizarTarea(em, t);
 			Tiene tiene2 = new Tiene();
 			tiene2.setFechaInicio(Calendar.getInstance());
-			//tiene2.setEstado(encontrarEstado(1));
+			// tiene2.setEstado(encontrarEstado(1));
 			tiene2.setEstado(est);
 			t.agregarTiene(tiene2);
-			actualizarTarea(em,t);
-			retorno=true;
+			actualizarTarea(em, t);
+			retorno = true;
 		}
-		
-		
 
 		return retorno;
 	}
 
-	
-	//agregar estado.......PROVISORIO
-	 public void agregarEstado(EntityManager em , Estado est) {
-		
-		em.persist(est);	
-		
+	// agregar estado.......PROVISORIO
+	public boolean agregarEstado(EntityManager em, Estado est) {
+		try{
+		em.persist(est);
+		return true;
+		}catch(Exception e){
+			e.printStackTrace();
+			return false;
+		}
 	}
 	// HABRIA QUE TENER UN METODO QUE RECIBA EL ESTADO ACTUAL DE LA TAREA
 	// Y DETERMINE LOS SIGUIENTES ESTADOS POSIBLES DEL FLUJO.
