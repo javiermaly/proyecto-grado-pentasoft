1dd88dd7df46442488fb9e6c566501395f05f018
reivaj <reivaj@reivaj-AOA150.lan>
2011-08-19 18:09:05 -0300

    TODO DE 0

--- SGTI_EJB/ejbModule/stateless/ClienteRemote.java
+++ SGTI_EJB/ejbModule/stateless/ClienteRemote.java
@@ -9,10 +9,10 @@ import beans.Cliente;
 @Remote
 public interface ClienteRemote {
 	
-	public void agregarCliente(Cliente c);	
-	public void eliminarCliente(int ced);	
+	public boolean agregarCliente(Cliente c);	
+	public boolean eliminarCliente(int ced);	
 	public List<Cliente> listarClientes();	
-	public void encontrarCliente(int ced);	
-	public void actualizarCliente(Cliente u);
+	public Cliente encontrarCliente(int ced);	
+	public Cliente actualizarCliente(Cliente u);
 	
 }
--- SGTI_EJB/ejbModule/stateless/ManagerU.java
+++ SGTI_EJB/ejbModule/stateless/ManagerU.java
@@ -7,51 +7,66 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import beans.Usuario;
-import manager.ManagerUsuario;
+
 
 
 
 @Stateless
 public class ManagerU implements UsuarioRemote{
 
-	@PersistenceContext(unitName ="SGTI_JPA")
-	private EntityManager em;
-	
-	ManagerUsuario mu = new ManagerUsuario();//manager usuario del jpa que maneja los beans	
-	private List<Usuario> listaUsuarios;
-	
-	
-	public void agregarUsuario(Usuario u){		
-		if(mu.altaUsuario(em, u)){//devuelve un booleano, ver como lo vamos a manejar
-			System.out.println(" usuario ingresado en la BD");
-		}else{
-			System.out.println(" EERRORRR INGRESO DE USUARIO POR ALGUN MOTIVO");
-		} 		
-	}
-	
-	public void eliminarUsuario(int ced){
-		Usuario u;
-		u = mu.encontrarUsuario(em, ced);		
-		if (u!=null){			
-			mu.eliminarUsuario(em, u);
-			System.out.println("USUARIO "+u.getNombre() +" eliminadoo ");
-		}else{
-			System.out.println("no existe usuario en la BD");
-		}		
-	}		
-	
-	public List<Usuario> listarUsuarios() {		
-		listaUsuarios= mu.traerTodosUsuarios(em);
-		return listaUsuarios;
-	}
-	
-	public void encontrarUsuario(int ced){
-		mu.encontrarUsuario(em, ced);
-	}
-	
-	public void actualizarUsuario(Usuario u){
-		mu.actualizarUsuario(em, u);		
-	}
-	
-		
+        @PersistenceContext(unitName ="SGTI_JPA")
+        private EntityManager em;
+        
+         
+        private List<Usuario> listaUsuarios;
+        
+        
+        public boolean agregarUsuario(Usuario u){               
+                try {                   
+                        em.persist(u);                  
+                        return true;
+
+                } catch (Exception e) {
+                        e.printStackTrace();
+                        return false;
+                }         
+        }
+        
+        public boolean eliminarUsuario(int ced){
+                Usuario u;
+                u = encontrarUsuario(ced);              
+                if (u!=null){                   
+                        try{
+                                em.remove(u);
+                                return true;
+                        }catch(Exception e ){
+                                e.printStackTrace();
+                                return false;
+                        }
+                }
+                else
+                        return false;
+        }               
+        
+        public List<Usuario> listarUsuarios() {         
+                List<Usuario> todos= em.createNamedQuery("todosUsuarios").getResultList();
+                return todos;
+        }
+        
+        public Usuario encontrarUsuario(int ced){
+                Usuario u = em.find(Usuario.class, ced);
+                return u;
+        }
+        
+        public boolean actualizarUsuario(Usuario u){
+                try{
+                        em.merge(u);
+                        return true;
+                }catch(Exception e){
+                        e.printStackTrace();
+                        return false;
+                }
+        }
+        
+                
 }
