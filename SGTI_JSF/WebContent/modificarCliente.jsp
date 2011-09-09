f41a1d3cc544904ccf2f48f2beb72829b3cb2ed5
reivaj <reivaj@reivaj-AOA150.lan>
2011-09-09 13:03:47 -0300

    Modificar cliente

---
+++ SGTI_JSF/WebContent/modificarCliente.jsp
@@ -0,0 +1,58 @@
+<%@ taglib prefix="f" uri="http://java.sun.com/jsf/core"%>
+<%@ taglib prefix="h" uri="http://java.sun.com/jsf/html"%>
+
+
+
+<f:view>
+	<f:subview id="header">
+		<%@include file="header.jsp"%>
+	</f:subview>
+	<h:form>
+		<h1>Modificar Cliente</h1>
+		<table>
+			<tr>
+				<td><h:outputText> Cedula o RUT del Cliente </h:outputText>
+				</td>
+				<td><h:inputText id="cedrut" value="#{ClienteBean.cedRut}"
+						required="true"></h:inputText>
+				</td>
+				<td><h:commandButton value="Aceptar"
+						action="#{ClienteBean.buscarCliente}"></h:commandButton>
+				</td>
+			</tr>
+		</table>
+	</h:form>
+
+	<h:form>
+		<table border="1">
+			<tr>
+				<th>Cédula / RUT: </th>
+				<th><h:inputText value="#{ClienteBean.cedRut}"></h:inputText></th>
+			</tr>
+			<tr>
+				<th>Nombre Completo / Razón Social:</th>
+				<td><h:inputText value="#{ClienteBean.nombreRazSocial}"></h:inputText>
+				</td>
+			</tr>
+			<tr>
+				<th>Teléfono</th>
+				<th><h:inputText value="#{ClienteBean.telefono}"></h:inputText></th>
+			</tr>
+						<tr>
+				<th>Fecha fin de Garantia</th>
+				<th><h:inputText value="#{ClienteBean.fechaFinGarantia}"><f:convertDateTime type="date"/></h:inputText></th> 
+			</tr> 
+			<tr>
+				<td><h:commandButton value="Aceptar"
+						action="#{ClienteBean.modificarCliente}"></h:commandButton></td>
+			</tr>
+		</table>
+	</h:form>
+
+
+</f:view>
+</body>
+</html>
+
+
+
--- SGTI_JSF/src/negocio/ClienteBean.java
+++ SGTI_JSF/src/negocio/ClienteBean.java
@@ -15,112 +15,145 @@ public class ClienteBean {
 	private String telefono;
 	private Date fechaFinGarantia;
 	private ClienteSession cliSession;
-	
-	ConexionEJB con = new ConexionEJB();	
-	FacadeRemote statelessFacade= con.conectar();
-	
-		
+
+	ConexionEJB con = new ConexionEJB();
+	FacadeRemote statelessFacade = con.conectar();
+
 	public long getCedRut() {
 		return cedRut;
 	}
+
 	public void setCedRut(long cedRut) {
 		this.cedRut = cedRut;
 	}
+
 	public ClienteSession getCliSession() {
 		return cliSession;
 	}
+
 	public void setCliSession(ClienteSession cliSession) {
 		this.cliSession = cliSession;
 	}
 
-
 	public String getNombreRazSocial() {
 		return nombreRazSocial;
 	}
+
 	public void setNombreRazSocial(String nombreRazSocial) {
 		this.nombreRazSocial = nombreRazSocial;
 	}
+
 	public String getDireccion() {
 		return direccion;
 	}
+
 	public void setDireccion(String direccion) {
 		this.direccion = direccion;
 	}
+
 	public String getTelefono() {
 		return telefono;
 	}
+
 	public void setTelefono(String telefono) {
 		this.telefono = telefono;
 	}
+
 	public Date getFechaFinGarantia() {
 		return fechaFinGarantia;
 	}
+
 	public void setFechaFinGarantia(Date fechaFinGarantia) {
 		this.fechaFinGarantia = fechaFinGarantia;
 	}
-	
-	
-//	//buscador para el Abrir Tarea
-//	public String buscarClienteAbrirTarea(){
-//		Cliente c = new Cliente();
-//		c = statelessFacade.buscarCliente(cedRut);
-//		if(c!=null){			
-//			cliSession.setClienteSession(c);
-//			System.out.println(cliSession.getClienteSession().getNombre_RazonSocial());
-//			System.out.println("cliente encontrado y puesto en la session");
-//		
-//			return "clienteEcontradoAbrirTarea";
-//		}else{
-//			System.out.println("cliente nulo!!");
-//			return "clienteNoEncontrado";
-//		}	
-//	}
-	
-	//buscador para Clientes
-	public String buscarCliente(){
+
+	// //buscador para el Abrir Tarea
+	// public String buscarClienteAbrirTarea(){
+	// Cliente c = new Cliente();
+	// c = statelessFacade.buscarCliente(cedRut);
+	// if(c!=null){
+	// cliSession.setClienteSession(c);
+	// System.out.println(cliSession.getClienteSession().getNombre_RazonSocial());
+	// System.out.println("cliente encontrado y puesto en la session");
+	//
+	// return "clienteEcontradoAbrirTarea";
+	// }else{
+	// System.out.println("cliente nulo!!");
+	// return "clienteNoEncontrado";
+	// }
+	// }
+
+	// buscador para Clientes
+	public String buscarCliente() {
 		Cliente c = new Cliente();
 		c = statelessFacade.buscarCliente(cedRut);
-		if(c!=null){			
+		if (c != null) {
 			cliSession.setClienteSession(c);
-			System.out.println(cliSession.getClienteSession().getNombre_RazonSocial());
+			System.out.println(cliSession.getClienteSession()
+					.getNombre_RazonSocial());
 			System.out.println("cliente encontrado y puesto en la session");
-		
+
 			return "clienteEcontrado";
-		}else{
-//			cliSession.clienteSession=null;
-//			Cliente c2=new Cliente();
-//			c2.setCedRut(cedRut);
-//			cliSession.setClienteSession(c);
-//			cliSession.clienteSession.setCedRut(cedRut);
-//			
-//			cliSession.setClienteSession(c2);
+		} else {
+			// cliSession.clienteSession=null;
+			// Cliente c2=new Cliente();
+			// c2.setCedRut(cedRut);
+			// cliSession.setClienteSession(c);
+			// cliSession.clienteSession.setCedRut(cedRut);
+			//
+			// cliSession.setClienteSession(c2);
 			System.out.println("cliente nulo!!");
 			return "clienteNoEncontrado";
-		}	
+		}
 	}
-	
-	public String altaCliente(){
-		//long ceduln=getCedRut();
-		System.out.println("veo si llega la cedula: "+getCedRut());
-		System.out.println("veo si llega la cedula: "+cedRut);
-		Cliente c= new Cliente();
-		//c=cliSession.getClienteSession();
+
+	public String altaCliente() {
+		// long ceduln=getCedRut();
+		System.out.println("veo si llega la cedula: " + getCedRut());
+		System.out.println("veo si llega la cedula: " + cedRut);
+		Cliente c = new Cliente();
+		// c=cliSession.getClienteSession();
 		c.setCedRut(getCedRut());
 		c.setDireccion(getDireccion());
 		c.setNombre_RazonSocial(getNombreRazSocial());
 		c.setTelefono(getTelefono());
-		if(!(fechaFinGarantia==null)){
-			Calendar cal=Calendar.getInstance();
+		if (!(fechaFinGarantia == null)) {
+			Calendar cal = Calendar.getInstance();
 			cal.setTime(fechaFinGarantia);
 			c.setFechaFinGarantia(cal);
 		}
-		System.out.println("altacliente en clienteBean"+c.getCedRut());
+		System.out.println("altacliente en clienteBean" + c.getCedRut());
 		System.out.println(c.getNombre_RazonSocial());
-		if(statelessFacade.altaCliente(c)){
+		if (statelessFacade.altaCliente(c)) {
 			return "altaClienteOK";
-		}
-		else
+		} else
 			return "falloAltaCliente";
 	}
-	
+
+	public String modificarCliente() {
+		System.out.println("modificar cliente");
+		Cliente c = statelessFacade.buscarCliente(getCedRut());
+		
+		if (!(c == null)) {
+			c.setCedRut(getCedRut());
+			c.setNombre_RazonSocial(getNombreRazSocial());
+			c.setTelefono(getTelefono());
+			//c.setDireccion(getDireccion());
+			if (!(fechaFinGarantia == null)) {
+				Calendar cal = Calendar.getInstance();
+				cal.setTime(fechaFinGarantia);
+				c.setFechaFinGarantia(cal);
+			}
+			cliSession.setClienteSession(c);
+			if(!((statelessFacade.modificarCliente(c))==null)){
+				return "clienteModificado";
+			}
+			else
+				return "errorModificarCliente";
+
+		}
+		else 
+			return "clienteNoExiste";
+
+	}
 }
