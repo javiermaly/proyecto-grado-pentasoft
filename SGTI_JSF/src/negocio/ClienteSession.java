7bef88e7820f13ce6228b4f45d41ade726fd863d
plindiman <plindiman@localhost6.localdomain6>
2011-09-09 23:05:24 -0300

    mostrar fecha en BajaCliente funcionando, falta arreglar detalle cuando primero muestra un cliente que teine fecha y luego uno que no.. queda la fecha mostrada del primero

--- SGTI_JSF/src/negocio/ClienteSession.java
+++ SGTI_JSF/src/negocio/ClienteSession.java
@@ -1,10 +1,23 @@
 package negocio;
 
+
+
+import java.util.Date;
+import java.util.Calendar;
+
 import beans.Cliente;
 
 public class ClienteSession {
 
 	public Cliente clienteSession;
+	public Date fechaGarant;
+
+
+	
+//	public ClienteSession() {
+//		Calendar fechG= clienteSession.getFechaFinGarantia();
+//		fechaGarant = fechG.getTime();
+//	}
 
 	public Cliente getClienteSession() {
 		return clienteSession;
@@ -14,4 +27,24 @@ public class ClienteSession {
 		System.out.println("set de Cliente session");
 		this.clienteSession = clienteSession;
 	}
+	
+
+	public Date getFechaGarant() {
+		return fechaGarant;
+	}
+
+	public void setFechaGarant(Date fechaGarant) {
+		
+		this.fechaGarant = fechaGarant;
+	}
+
+	public Date dameFechaDate() {
+		//clienteSession= new Cliente();
+		Calendar fechG= clienteSession.getFechaFinGarantia();
+		Date fecha = fechG.getTime();
+		return fecha;
+	}
+	
+	
+	
 }
